47fee5f7e74cb977dd9ad8af648fff3e2cba0f88
a-chikara <aiba102chikara@gmail.com>
2021-03-23 20:33:25 +0900

    Active Storageの導入

--- app/controllers/yumememos_controller.rb
+++ app/controllers/yumememos_controller.rb
@@ -1,4 +1,12 @@
 class YumememosController < ApplicationController
   def index
   end
+
+
+
+  private
+
+  def meno_params
+    params.require(:memo).permit(:image)
+  end
 end
--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,2 +1,3 @@
 class Memo < ApplicationRecord
+  has_one_attached :image
 end
