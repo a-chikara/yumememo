1cbf55d41d8273ad4b5037953dba682166e0267a
a-chikara <aiba102chikara@gmail.com>
2021-03-27 22:03:47 +0900

    フロント調整

--- app/models/deadline.rb
+++ app/models/deadline.rb
@@ -1,9 +1,15 @@
 class Deadline < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '1~2日で発送' },
-    { id: 3, name: '2~3日で発送' },
-    { id: 4, name: '4~7日で発送' },
+    { id: 2, name: '1week' },
+    { id: 3, name: '1month' },
+    { id: 4, name: '3month' },
+    { id: 5, name: '6month' },
+    { id: 6, name: '1year' },
+    { id: 7, name: '3years' },
+    { id: 8, name: '5years' },
+    { id: 9, name: '10years' },
+    { id: 10, name: 'Other' },
   ]
 
 
